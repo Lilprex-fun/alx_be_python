69468b1d10dad5816945ed2ff2cc13fa8afefad8
Lilprex-fun <kimangaprecious@gmail.com>
2025-12-07 13:40:01 +0300

    Fix simple interest calculation and output formatting

--- simple_interest.py
+++ simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3.5
-simple interest formula = P * R * T 
+
 interest = principal * rate * time
-print("Simple intrest is {interest}")
+print(f"Simple intrest is {interest}")
